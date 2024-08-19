a07f64b41acf15211d9c45fcdb42973b3fcd665d
araobp <araobp@users.noreply.github.com>
2024-08-19 20:17:50 +0900

    update

--- DigitalHuman/Assets/Scripts/ChatbotController.cs
+++ DigitalHuman/Assets/Scripts/ChatbotController.cs
@@ -11,13 +11,13 @@ public class ChatbotController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnEnable()
@@ -33,10 +33,21 @@ public class ChatbotController : MonoBehaviour
     private void GetKeyInput(char obj)
     {
         Debug.Log(obj);
-        switch (obj) {
-            case '1':  // SitDown
-                m_LadyBotAnimator.SetBool("sitDown", true);
+        switch (obj)
+        {
+            case '1':  // sitDown
+                m_LadyBotAnimator.SetTrigger("sitDown");
                 break;
+            case '2':  // standUp
+                m_LadyBotAnimator.SetTrigger("standUp");
+                break;
+            case '3':  // speak
+                m_LadyBotAnimator.SetTrigger("speak");
+                break;
+            case '4':  // stopSpeaking
+                m_LadyBotAnimator.SetTrigger("stopSpeaking");
+                break;
+
         }
     }
 }
