7d774f2c4737e72e39ddefb26bcbdee9abbda627
araobp <araobp@users.noreply.github.com>
2024-08-20 20:18:03 +0900

    update

--- DigitalHuman/Assets/Scripts/ChatbotController.cs
+++ DigitalHuman/Assets/Scripts/ChatbotController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,8 +12,13 @@ public class ChatbotController : MonoBehaviour
     [SerializeField] Camera m_Camera3;
     [SerializeField] Camera m_Camera4;
     [SerializeField] Camera m_Camera5;
+    [SerializeField] GameObject m_Screen;
 
     List<Camera> m_Cameras = new List<Camera>();
+
+    int m_Idx = 0;
+
+    List<Object> m_Pictures = null;
 
     void selectCamera(Camera camera)
     {
@@ -32,11 +38,18 @@ public class ChatbotController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
         m_Cameras.Add(m_Camera1);
         m_Cameras.Add(m_Camera2);
         m_Cameras.Add(m_Camera3);
         m_Cameras.Add(m_Camera4);
         m_Cameras.Add(m_Camera5);
+
+        var pictures = Resources.LoadAll("Panorama", typeof(Texture2D)).ToList();
+        m_Pictures = pictures.OrderBy(x => x.name).ToList();
+        Texture2D tex = (Texture2D)m_Pictures[0];
+        m_Screen.GetComponent<Renderer>().material.SetTexture("_Texture2D", tex);
     }
 
     // Update is called once per frame
@@ -53,6 +66,28 @@ public class ChatbotController : MonoBehaviour
     void OnDisable()
     {
         Keyboard.current.onTextInput -= GetKeyInput;
+    }
+
+    public void Forward()
+    {
+        m_Idx += 1;
+        if (m_Idx >= m_Pictures.Count - 1)
+        {
+            m_Idx = m_Pictures.Count - 1;
+        }
+        Texture2D tex = (Texture2D)m_Pictures[m_Idx];
+        m_Screen.GetComponent<Renderer>().material.SetTexture("_Texture2D", tex);
+    }
+
+    public void Back()
+    {
+        m_Idx -= 1;
+        if (m_Idx < 0)
+        {
+            m_Idx = 0;
+        }
+        Texture2D tex = (Texture2D)m_Pictures[m_Idx];
+        m_Screen.GetComponent<Renderer>().material.SetTexture("_Texture2D", tex);
     }
 
     private void GetKeyInput(char obj)
@@ -87,7 +122,12 @@ public class ChatbotController : MonoBehaviour
             case '9':  // Camera5
                 selectCamera(m_Camera5);
                 break;
-
+            case 'f':
+                Forward();
+                break;
+            case 'b':
+                Back();
+                break;
         }
     }
 }
