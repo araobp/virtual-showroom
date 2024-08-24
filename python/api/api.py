a8a7a51519abd71a5fe3c35ac788473413510a3e
araobp <araobp@users.noreply.github.com>
2024-08-24 11:18:25 +0900

    update

--- python/api/api.py
+++ python/api/api.py
@@ -1,7 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+import chat
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def hello_world():
   return "Hello, Virtual Showroom!"
+
+@main.route("/chat")
+def chat():
+  query = request.args.get("query", default=None, type=str)
+  resp = chat.query(query)
+  return jsonify(resp)
